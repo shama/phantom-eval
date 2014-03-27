88f77de65c1918e6456d1f3932a164a7b20a0e19
Kyle Robinson Young <kyle@dontkry.com>
2014-03-27 13:44:31 -0700

    A better way to detect wrapping function

--- index.js
+++ index.js
@@ -7,23 +7,34 @@ module.exports = function(url, fn, done) {
   var script = path.join(__dirname, 'phantom.js')
   var key = '--- ' + +Date.now() + ' ---'
 
+  function isWrapper(node) {
+    return (
+      node.type === 'FunctionExpression' &&
+      node.parent &&
+      node.parent.arguments[0] &&
+      node.parent.arguments[0].value === key
+    )
+  }
+
   if (typeof fn === 'function') {
-    var first = true
     var output = ''
-    falafel('(' + fn + '())', function(node) {
+    falafel('(' + fn + '("' + key + '"))', function(node) {
+      // Prefix a ; on returns for those who dont use ;
+      if (node.type === 'ReturnStatement') {
+        node.update(';' + node.source())
+      }
       // Pull out the contents of the surrounding body function
-      if (first === true && node.type === 'FunctionExpression') {
+      if (isWrapper(node)) {
         for (var i = 0; i < node.body.body.length; i++) {
           output += node.body.body[i].source()
         }
-        first = false
       }
     })
     fn = output
   }
   
   // Serialize and run phantomjs
-  fn = JSON.stringify(fn.toString().replace(/\n|\r\n/g, ';'))
+  fn = JSON.stringify(fn.toString().replace(/\n|\r\n/g, ''))
   execFile(binPath, [script, url, fn, key], function(err, stdout, stderr) {
     if (err || stderr) return done(err || new Error(stderr), null, stdout)
     var data = stdout.split(key).slice(1, -1).join('').replace(/\n|\r\n/g, '')
